16c01b9fd611c6e34cefdcded667f72e8377f4b5
8debug <297179121@qq.com>
2021-08-05 16:13:25 +0800

    修正爬虫

--- com/his/His.py
+++ com/his/His.py
@@ -1,12 +1,15 @@
 from copy import copy
 
-import xlrd
+from openpyxl import Workbook
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from xlutils.copy import copy
 from selenium.webdriver.support import expected_conditions as EC
+
+global wb
+global ws
 
 
 class His:
@@ -16,13 +19,18 @@ class His:
         """
         # 实现无可视化界面
         options = Options()
-        # options.add_argument('--headless')
-        # options.add_argument('--disable-gpu')
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
         options.add_experimental_option('excludeSwitches', ['enable-automation'])
         self.browser = webdriver.Chrome(executable_path='D:\project\pythonspace\spiderEventhing\chromedriver.exe',
                                         options=options)
         self.browser.maximize_window()
         # self.browser.implicitly_wait(3)  # 全局隐式等待10秒
+        global wb
+        wb = Workbook()
+
+        global ws
+        ws = wb.active
 
     def open(self):
         try:
@@ -34,45 +42,42 @@ class His:
             for a in rows:
                 href = a.get_property('href')
                 name = a.text
-                code = str.replace(href, ".html", "")
-                code = str.replace(code, "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/", '')
-                level = "-".join([code, name])
-                self.level_2(level, href)
-                break
+                # code = str.replace(href, ".html", "")
+                # code = str.replace(code, "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/", '')
+                # level = code+"-"+name
+                self.level_2(name, href)
         finally:
+            self.excel_colse()
             self.browser.quit()
         return self
 
-    def level_2(self, parent_leven, url):
-        print(url)
+    def level_2(self, content, url):
         self._open_switch(url)
         self.util("//a[text()='京ICP备05034670号']")
         # 获取指定节点的父节点的所有向下兄弟节点的子节点a元素
         xpath_tr = "//td[text()='统计用区划代码']/parent::tr/following-sibling::*"
-        rows = self.browser.find_elements_by_xpath("//td[text()='统计用区划代码']/parent::tr/following-sibling::*//a")
-        if len(rows) > 0:
-            array_tr = self.browser.find_elements_by_xpath(xpath_tr)
-            for tr in array_tr:
-                first = tr.find_elements_by_xpath(".//td")[0]
-                last = tr.find_element_by_xpath(".//td[last()]")
-                code = first.find_element_by_xpath(".//a").text
-                href = first.find_element_by_xpath(".//a").get_property('href')
-                name = last.text
-                level = ",".join([parent_leven, code + "-" + name])
-                self.level_2(level, href)
-        else:
-            array_tr = self.browser.find_elements_by_xpath(xpath_tr)
-            for tr in array_tr:
-                first = tr.find_elements_by_xpath(".//td")[0]
-                last = tr.find_element_by_xpath(".//td[last()]")
-                code = first.text
-                name = last.text
-                if name.find("多福巷社区居委会") > -1:
-                    True
-                level = ",".join([parent_leven, code + "-" + name])
+        array_tr = self.browser.find_elements_by_xpath(xpath_tr)
+        for tr in array_tr:
+            array_td = tr.find_elements_by_xpath("./td")
+            td_first = array_td[0]
+            td_last = array_td[len(array_td) - 1]
+            array_a = tr.find_elements_by_xpath(".//a")
+            if len(array_a) > 0:
+                a_first = td_first.find_element_by_xpath(".//a")
+                code = a_first.text
+                href = a_first.get_property('href')
+                name = td_last.text
+                level = "-".join([content, code + "-" + name])
                 print(level)
-            self._close_switch()
-            print("关闭")
+                self.excel_append(level)
+                self.level_2(level, href)
+            else:
+                code = td_first.text
+                name = td_last.text
+                level = ",".join([content, code + "-" + name])
+                print(level)
+                self.excel_append(level)
+        self._close_switch()
 
     def util(self, xpath):
         # print("util===", xpath)
@@ -102,6 +107,13 @@ class His:
         all_handles = self.browser.window_handles  # 获取全部页面句柄
         self.browser.switch_to.window(all_handles.pop())  # 打开 最新弹出的页面
         self.browser.get(url)
+
+    def excel_append(self, content):
+        # 可以使用append插入一行数据
+        ws.append([content])
+
+    def excel_colse(self):
+        wb.save('D:/project/pythonspace/spiderEventhing/2020年统计用区划代码和城乡划分代码.xlsx')
 
 
 his = His()
