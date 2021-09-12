accae828647b0066113cda3daa820e7c1d02fc03
297179121 <297179121@qq.com>
2021-09-12 23:01:20 +0800

    fix bugs

--- com/repair/Parts.py
+++ com/repair/Parts.py
@@ -1,5 +1,7 @@
 from time import sleep
 
+from selenium.common.exceptions import NoSuchElementException
+
 from com.repair.Repair import Repair
 
 
@@ -10,56 +12,68 @@ class Parts(Repair):
         self.s = "@"
 
     def set_current_page(self):
-        str_page = self.browser.find_element_by_xpath('//li[@class="paginate_button active"]/a').text
+        str_page = self.current_page()
         self.page = int(str_page)
+
+    def current_page(self):
+        str_page = self.browser.find_element_by_xpath('//li[@class="paginate_button active"]/a').text
         print("当前页面", str_page)
+        return str_page
 
     def parts(self):
-        xpath = "//a[@href='/qxgl/part/list.do']"
-        a = self.browser.find_element_by_xpath(xpath)
-        a.click()
-        self.util("//div[text()='零件管理']")
-        self.spider_data()
-        self.next_page()
-        self.excel_colse("零件")
-        self.close_browser()
+        try:
+            a = self.browser.find_element_by_xpath("//a[@href='/qxgl/part/list.do']")
+            a.click()
+            self.util("//div[text()='零件管理']")
+            self.spider_data()
+            self.next_page()
+            self.excel_colse("零件")
+            self.close_browser()
+        except Exception as e:
+            print(e)
+            # self.close_browser()
 
     def next_page(self):
         """
         翻页
         :return:
         """
-        # 滚到底部
-        self.scroll_bottom()
         # 当前页
         self.set_current_page()
         page_next = self.browser.find_element_by_xpath("//a[text()='下一页']/parent::li")
         if "disabled" not in page_next.get_attribute("class"):
             page_next.find_element_by_xpath("./a").click()
             page = str(self.page + 1)
-            self.util("//li[@class='paginate_button active']/a[text()="+page+"]")
+            self.util("//li[@class='paginate_button active']/a[text()=" + page + "]")
             self.spider_data()
             self.next_page()
         else:
             return
+
+    def util_list_page(self):
+        self.util("//ul[@class='pagination']")
 
     def spider_data(self):
         """
         爬取数据
         :return:
         """
+        self.util_list_page()
+        current_page = self.current_page()
         array = self.browser.find_elements_by_xpath("//table[@id='part-table']/tbody/tr")
-        for tr in array:
+        for i in range(0, len(array)):
+            tr = array[i]
             array_td = tr.find_elements_by_xpath("./td")
             part_id = array_td[9].find_elements_by_xpath("./a")[0].get_attribute("part-id")
             detail = self.detail(part_id)
-            print(detail)
+            print("第{}页，第{}行，详情地址{}".format(current_page, (i+1), detail))
             self.excel_append(detail)
 
     def detail(self, part_id):
         url = 'http://60.16.11.38:93/qxgl/part/edit.do?part.id=' + part_id
         self.open_switch(url)
         self.util("//div[text()='零件信息']")
+        self.util("//div[@id='statics-table1_paginate']/ul[@class='pagination']")
         # 库房选择
         sm = self.browser.find_element_by_xpath("//*[@id='select2-chosen-2']").text
         # 零件名称
@@ -82,10 +96,27 @@ class Parts(Repair):
         time_limit = self.browser.find_element_by_xpath("//*[@id='select2-chosen-5']").text
         # 规格
         standard = self.browser.find_element_by_xpath("//*[@id='part_specification']").get_property("value")
+        # 条形码
+        try:
+            barcode = self.browser.find_element_by_xpath("//table[@id='statics-table1']/tbody/tr[1]/td[6]").text
+        except NoSuchElementException as e:
+            barcode = ''
 
         detail = self.s.join(
-            ["库房选择", sm, "零件名称", part_name, "所属配件架", shelf, "最低库存", stock_min, "品牌", brand,
-             "货商", supplier, "备注", remark, "零件类型", part_type, "单位", unit, "使用期限", time_limit, "规格", standard])
+            ["库房选择", sm,
+             "零件名称", part_name,
+             "所属配件架", shelf,
+             "最低库存", stock_min,
+             "品牌", brand,
+             "货商", supplier,
+             "备注", remark,
+             "零件类型", part_type,
+             "单位", unit,
+             "使用期限", time_limit,
+             "规格", standard,
+             "条形码", barcode
+             ]
+        )
 
         self.close_switch()
 
